6996e54103deebbed9387063dd3ea17ca61cd90f
vladimir <vlado.sedlar@gmail.com>
2024-07-03 23:24:27 +0200

    nvim(zellij,rm): remove zellij plugin

--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -11,14 +11,14 @@ return {
     "mrjones2014/smart-splits.nvim",
     event = "VeryLazy",
   },
-  {
-    "https://github.com/fresh2dev/zellij.vim.git",
-    lazy = false,
-    keys = {
-      { "<M-h>", ":ZellijNavigateLeft<CR>", mode = { "n", "i" }, silent = true, { silent = true, noremap = true } },
-      { "<M-j>", ":ZellijNavigateDown<CR>", mode = { "n", "i" }, silent = true, { noremap = true } },
-      { "<M-k>", ":ZellijNavigateUp<CR>", mode = { "n", "i" }, silent = true, { noremap = true } },
-      { "<M-l>", ":ZellijNavigateRight<CR>", mode = { "n", "i" }, silent = true, { noremap = true } },
-    },
-  },
+  -- {
+  --   "https://github.com/fresh2dev/zellij.vim.git",
+  --   lazy = false,
+  --   keys = {
+  --     { "<M-h>", ":ZellijNavigateLeft<CR>", mode = { "n", "i" }, silent = true, { silent = true, noremap = true } },
+  --     { "<M-j>", ":ZellijNavigateDown<CR>", mode = { "n", "i" }, silent = true, { noremap = true } },
+  --     { "<M-k>", ":ZellijNavigateUp<CR>", mode = { "n", "i" }, silent = true, { noremap = true } },
+  --     { "<M-l>", ":ZellijNavigateRight<CR>", mode = { "n", "i" }, silent = true, { noremap = true } },
+  --   },
+  -- },
 }
