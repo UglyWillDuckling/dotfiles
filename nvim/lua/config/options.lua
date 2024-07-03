3b2e32f1ac4d3efb321f3e8b711595dbd9080858
vladimir <vlado.sedlar@gmail.com>
2024-07-03 23:20:31 +0200

    nvim(config,chars): disable list special chars

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
+local opt = vim.opt
+
+opt.list = false -- Hide special chars
+
 -- CHADtree
 -- https://github.com/ms-jpq/chadtree/blob/chad/docs/CONFIGURATION.md
 -- https://github.com/ms-jpq/chadtree/blob/chad/docs/THEME.md
