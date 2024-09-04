7b509a1cb9a107492b9871108a1219a61705712b
vladimir <vlado.sedlar@gmail.com>
2024-09-04 22:30:50 +0200

    nvim: remove markdown from lang list

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -16,7 +16,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "lazyvim.plugins.extras.test.core" },
     -- import/override with your plugins
