8156914298e15a72f62480840371d8777075e7d3
aviv-vladimirsedlar <vlado.sedlar@gmail.com>
2024-09-13 10:06:32 +0200

    nvim(lsp): disable some mappings from lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,4 +1,14 @@
 return {
+  -- LSP keymaps
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = { "<a-p>", false }
+      keys[#keys + 1] = { "<a-n>", false }
+      keys[#keys + 1] = { "<C-K>", false }
+    end,
+  },
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
