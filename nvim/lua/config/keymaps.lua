e679972efd9784434b4532ff0cb8620aa3d41b4a
aviv-vladimirsedlar <vladimir.sedlar-ext@aviv-group.com>
2024-10-07 11:42:04 +0200

    nvim(codeium,map): remap accept word to <C-e>

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -69,7 +69,7 @@ vim.keymap.set("i", "<C-l>", function()
   return vim.fn["codeium#AcceptNextLine"]()
 end, { expr = true, silent = true })
 
-vim.keymap.set("i", "<C-i>", function()
+vim.keymap.set("i", "<C-e>", function()
   return vim.fn["codeium#AcceptNextWord"]()
 end, { expr = true, silent = true })
 
