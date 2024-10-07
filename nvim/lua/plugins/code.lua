5f6824095b81b6fdfcc26bdc6a606753d7d7b29e
aviv-vladimirsedlar <vladimir.sedlar-ext@aviv-group.com>
2024-10-07 11:39:02 +0200

    nvim(markdown): add peek plugin

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -12,4 +12,14 @@ return {
       "kana/vim-textobj-user",
     },
   },
+  {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+      require("peek").setup()
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+  },
 }
