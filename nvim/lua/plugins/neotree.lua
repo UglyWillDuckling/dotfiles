b9ab76fef7368a42e57b7fd884124ea865991a25
aviv-vladimirsedlar <vladimir.sedlar-ext@aviv-group.com>
2024-10-28 10:27:34 +0100

    nvim(tree): add nvim-tree

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,5 +1,16 @@
 return {
   {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup({})
+    end,
+  },
+  {
     "PhilRunninger/nerdtree-visual-selection",
     event = "VeryLazy",
   },
