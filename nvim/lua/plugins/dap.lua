b629fe4eb06731632c9bc6a15ea26ec7b214050b
vladimir <vladokiller33@gmail.com>
2025-05-14 21:44:32 +0200

    nvim(php): add pest/phpunit support in neotest

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -182,7 +182,16 @@ return {
             'nvim-lua/plenary.nvim',
             'antoinemadec/FixCursorHold.nvim',
             'nvim-treesitter/nvim-treesitter',
+            'olimorris/neotest-phpunit',
+            'V13Axel/neotest-pest',
         },
+        config = function()
+            require('neotest').setup {
+                adapters = {
+                    require 'neotest-pest',
+                },
+            }
+        end,
     },
     {
         'vim-test/vim-test',
