5b309cb2f31e7c92bcf37e139bba9b07914b46f5
vladimir <vlado.sedlar@gmail.com>
2025-05-28 23:45:03 +0200

    nvim: rm duplicate clangd init

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -13,7 +13,7 @@ return {
 
             vim.lsp.enable 'solargraph'
             vim.lsp.enable 'arduino_language_server'
-            vim.lsp.enable 'clangd'
+            -- vim.lsp.enable 'clangd'
 
             require('lspconfig').pyright.setup {
                 settings = {
