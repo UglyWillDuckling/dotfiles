380e049d9857ee735d26d4a301813628ac676c48
vladimir <vlado.sedlar@gmail.com>
2025-06-02 15:24:47 +0200

    nvim: caddyfile support

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -1,23 +1,28 @@
 return {
-  {
-    "glts/vim-textobj-comment",
-    lazy = true,
-    event = "VeryLazy",
-    dependencies = {
-      "kana/vim-textobj-user",
+    {
+        'isobit/vim-caddyfile',
+        lazy = true,
+        event = 'VeryLazy',
     },
-  },
-  {
-    "toppair/peek.nvim",
-    event = { "VeryLazy" },
-    build = "deno task --quiet build:fast",
-    config = function()
-      require("peek").setup()
-      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-    end,
-  },
-  {
-    "sigmasd/deno-nvim",
-  },
+    {
+        'glts/vim-textobj-comment',
+        lazy = true,
+        event = 'VeryLazy',
+        dependencies = {
+            'kana/vim-textobj-user',
+        },
+    },
+    {
+        'toppair/peek.nvim',
+        event = { 'VeryLazy' },
+        build = 'deno task --quiet build:fast',
+        config = function()
+            require('peek').setup()
+            vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+            vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+        end,
+    },
+    {
+        'sigmasd/deno-nvim',
+    },
 }
