0cfbdd245f411832acd7452ab16f7ebd622e116b
vladimir <vladokiller33@gmail.com>
2025-06-03 14:39:56 +0200

    nvim(blink): map 1-9 selection to <C-{1-9}>

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -20,6 +20,56 @@ return {
             keymap = {
                 ['<CR>'] = { 'accept', 'fallback' },
                 ['<C-\\>'] = { 'hide', 'fallback' },
+                ['<C-1>'] = {
+                    function(cmp)
+                        cmp.accept { index = 1 }
+                    end,
+                },
+                ['<C-2>'] = {
+                    function(cmp)
+                        cmp.accept { index = 2 }
+                    end,
+                },
+                ['<C-3>'] = {
+                    function(cmp)
+                        cmp.accept { index = 3 }
+                    end,
+                },
+                ['<C-4>'] = {
+                    function(cmp)
+                        cmp.accept { index = 4 }
+                    end,
+                },
+                ['<C-5>'] = {
+                    function(cmp)
+                        cmp.accept { index = 5 }
+                    end,
+                },
+                ['<C-6>'] = {
+                    function(cmp)
+                        cmp.accept { index = 6 }
+                    end,
+                },
+                ['<C-7>'] = {
+                    function(cmp)
+                        cmp.accept { index = 7 }
+                    end,
+                },
+                ['<C-8>'] = {
+                    function(cmp)
+                        cmp.accept { index = 8 }
+                    end,
+                },
+                ['<C-9>'] = {
+                    function(cmp)
+                        cmp.accept { index = 9 }
+                    end,
+                },
+                ['<C-0>'] = {
+                    function(cmp)
+                        cmp.accept { index = 10 }
+                    end,
+                },
                 ['<C-n>'] = { 'select_next', 'show' },
                 ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
                 ['<C-p>'] = { 'select_prev' },
