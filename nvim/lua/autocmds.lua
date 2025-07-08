99b5039473db435c3019e85b2f4a2b1e46165d83
vladimir <vladokiller33@gmail.com>
2025-07-08 13:55:33 +0200

    nvim: always apply zz when jumping to mark

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -60,8 +60,8 @@ vim.api.nvim_create_autocmd('BufReadPost', {
     callback = function(args)
         local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
         local line_count = vim.api.nvim_buf_line_count(args.buf)
+        vim.cmd 'normal! g`"zz' -- always do this
         if mark[1] > 0 and mark[1] <= line_count then
-            vim.cmd 'normal! g`"zz'
         end
     end,
 })
