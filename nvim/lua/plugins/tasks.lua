3c30f25d1fc7b49bf8051ea1d14ea5189085ff45
vladimir <vladokiller33@gmail.com>
2025-11-12 11:53:49 +0100

    nvim(tasks): update dooing config
    Add maps
    Improve current mappings
    Windows positioning

--- nvim/lua/plugins/tasks.lua
+++ nvim/lua/plugins/tasks.lua
@@ -1,34 +1,56 @@
 return {
-  "atiladefreitas/dooing",
-  config = function()
-    require("dooing").setup({
-      -- your custom config here (optional)
-      keymaps = {
-        toggle_window = "<leader>td",
-        new_todo = "i",
-        toggle_todo = "x",
-        delete_todo = "d",
-        delete_completed = "D",
-        close_window = "q",
-        undo_delete = "u",
-        add_due_date = "H",
-        remove_due_date = "r",
-        toggle_help = "?",
-        toggle_tags = "t",
-        toggle_priority = "p",
-        clear_filter = "c",
-        edit_todo = "e",
-        edit_tag = "e",
-        edit_priorities = "p",
-        delete_tag = "d",
-        search_todos = "/",
-        add_time_estimation = "T",
-        remove_time_estimation = "R",
-        import_todos = "I",
-        export_todos = "E",
-        remove_duplicates = "<leader>D",
-        open_todo_scratchpad = "<leader>p",
-      },
-    })
-  end,
+    'atiladefreitas/dooing',
+    config = function()
+        require('dooing').setup {
+            -- Window settings
+            window = {
+                width = 65, -- Width of the floating window
+                height = 23, -- Height of the floating window
+                position = 'bottom-left', -- 'right', 'left', 'top', 'bottom', 'center',
+                -- 'top-right', 'top-left', 'bottom-right', 'bottom-left'
+            },
+            keymaps = {
+                toggle_window = '<leader>tD', -- Toggle global todos
+                open_project_todo = '<leader>tt', -- Toggle project-specific todos
+                create_nested_task = '<S-n>', -- Create nested subtask under current todo
+                new_todo = 'i',
+                toggle_todo = 'x',
+                delete_todo = 'd',
+                delete_completed = 'D',
+                close_window = 'q',
+                undo_delete = 'u',
+                add_due_date = 'H',
+                remove_due_date = 'r',
+                toggle_help = '?',
+                toggle_tags = 't',
+                toggle_priority = 'p',
+                clear_filter = 'c',
+                edit_todo = 'e',
+                edit_tag = 'e',
+                edit_priorities = 'p',
+                delete_tag = 'd',
+                search_todos = '/',
+                add_time_estimation = 'T',
+                remove_time_estimation = 'R',
+                import_todos = 'I',
+                export_todos = 'E',
+                remove_duplicates = '<leader>D',
+                open_todo_scratchpad = 'c',
+            },
+            calendar = {
+                language = 'en',
+                icon = '🗓️',
+                keymaps = {
+                    previous_day = 'h',
+                    next_day = 'l',
+                    previous_week = 'k',
+                    next_week = 'j',
+                    previous_month = 'H',
+                    next_month = 'L',
+                    select_day = '<CR>',
+                    close_calendar = 'q',
+                },
+            },
+        }
+    end,
 }
