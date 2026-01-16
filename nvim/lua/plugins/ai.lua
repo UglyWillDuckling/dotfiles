e7a92e99ec554cd1b975a71599fa197f1ffcbca5
vladimir <vladokiller33@gmail.com>
2026-01-16 10:20:22 +0100

    nvim: configure llm to use groq

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -2,5 +2,18 @@ return {
     {
         'melbaldove/llm.nvim',
         dependencies = { 'nvim-neotest/nvim-nio' },
+        config = function()
+            require('llm').setup {
+                -- How long to wait for the request to start returning data.
+                timeout_ms = 10000,
+                services = {
+                    groq = {
+                        url = 'https://api.groq.com/openai/v1/chat/completions',
+                        model = 'llama-3.1-8b-instant',
+                        api_key_name = 'GROQ_API_KEY',
+                    },
+                },
+            }
+        end,
     },
 }
