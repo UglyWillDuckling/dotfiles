52da2947e3444ec8ef72f5824638b995263a8d4f
vladimir <vladokiller33@gmail.com>
2026-01-20 09:51:51 +0100

    nvim: add rhubarb plugin, fugitive dep

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,5 +1,12 @@
 return {
     {
         'tpope/vim-fugitive',
+        lazy = true,
+        event = 'VeryLazy',
+    },
+    {
+        'tpope/vim-rhubarb',
+        lazy = true,
+        event = 'VeryLazy',
     },
 }
