9fbef1e87d3365b9eacb20e78b414d95bd507b5b
vladimir <vlado.sedlar@gmail.com>
2026-04-26 22:30:26 +0200

    shell: update colors script

--- bash/colors.sh
+++ bash/colors.sh
@@ -1,9 +1,40 @@
 #!/usr/bin/env bash
 
-R='\033[0;31m'
-G='\033[0;32m'
-Y='\e[33m'
+set -v
 
-error() { echo -e "${R}$*\e[0m"; }
-success() { echo -e "${G}$*\e[0m"; }
-warning() { echo -e "${Y}$*\e[0m"; }
+# $1 is the color number
+# $* represents the text that we want to output
+color() { 
+    c=$1
+    shift
+    t="$*"
+    echo -e "\e[38;5;${c}m${t}\e[0m"; 
+}
+
+bold() {
+    echo -e "\e[1m$*\e[0m"; 
+}
+
+# by type
+error() { red "$*"; }
+success() { green "$*"; }
+warning() { yellow 3 "$*"; }
+
+# by color
+red() { color 1 "$*"; }
+green() { color 2 "$*"; }
+yellow() { color 3 "$*"; }
+blue() { color 4 "$*"; }
+
+hex_to_rgb() {
+    # Usage: hex_to_rgb "#FFFFFF"
+    #        hex_to_rgb "000000"
+    : "${1/\#}"
+    ((r=16#${_:0:2},g=16#${_:2:2},b=16#${_:4:2}))
+    printf '%s\n' "$r $g $b"
+}
+
+rgb_to_hex() {
+    # Usage: rgb_to_hex "r" "g" "b"
+    printf '#%02x%02x%02x\n' "$1" "$2" "$3"
+}
