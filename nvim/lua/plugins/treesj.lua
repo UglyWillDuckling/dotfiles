e8a61f14a0e97d45519d7225c2d88b3bab7ee317
vladimir <vlado.sedlar@gmail.com>
2026-05-04 19:15:13 +0200

    nvim(todo): add split it up todo

--- nvim/lua/plugins/treesj.lua
+++ nvim/lua/plugins/treesj.lua
@@ -4,6 +4,7 @@ return {
         'Wansmer/treesj',
         dependencies = 'nvim-treesitter',
         keys = {
+            -- TODO: check this out, split it up!!!
             { '<leader>cj', '<cmd>TSJToggle<cr>', desc = 'Join/split code block' },
         },
         opts = { use_default_keymaps = false },
