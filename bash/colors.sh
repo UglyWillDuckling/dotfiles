77374de1c2d36fd3bb64c462a38a7cc9cef6551d
vladimir <vladokiller33@gmail.com>
2026-05-05 11:04:47 +0200

    chore(shell): colors update

--- bash/colors.sh
+++ bash/colors.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-set -v
+# set -v
 
 # $1 is the color number
 # $* represents the text that we want to output
-color() { 
+color() {
     c=$1
     shift
     t="$*"
-    echo -e "\e[38;5;${c}m${t}\e[0m"; 
+    echo -e "\e[38;5;${c}m${t}\e[0m"
 }
 
 bold() {
-    echo -e "\e[1m$*\e[0m"; 
+    echo -e "\e[1m$*\e[0m"
 }
 
 # by type
@@ -29,8 +29,8 @@ blue() { color 4 "$*"; }
 hex_to_rgb() {
     # Usage: hex_to_rgb "#FFFFFF"
     #        hex_to_rgb "000000"
-    : "${1/\#}"
-    ((r=16#${_:0:2},g=16#${_:2:2},b=16#${_:4:2}))
+    : "${1/\#/}"
+    ((r = 16#${_:0:2}, g = 16#${_:2:2}, b = 16#${_:4:2}))
     printf '%s\n' "$r $g $b"
 }
 
